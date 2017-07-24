16189832f1bcd0ea632fbd65649ce2887c58f7cd
hainet <20400616+hainet@users.noreply.github.com>
2017-07-25 08:02:48 +0900

    動的SQLのサンプルを追加

--- src/main/java/com/hainet/mybatis/spring/boot/sample/mapper/PersonMapper.java
+++ src/main/java/com/hainet/mybatis/spring/boot/sample/mapper/PersonMapper.java
@@ -2,6 +2,7 @@ package com.hainet.mybatis.spring.boot.sample.mapper;
 
 import com.hainet.mybatis.spring.boot.sample.domain.Person;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -13,6 +14,10 @@ public interface PersonMapper {
     List<Person> findAll();
 
     Person findById(int id);
+
+    List<Person> findByNameAndAge(@Param("name") String name, @Param("age") int age);
+
+    List<Person> findByNames(List<String> names);
 
     int insert(Person person);
 
