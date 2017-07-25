ad8efac12299363c189b3bc4cff2c95d1dd2a022
hainet <20400616+hainet@users.noreply.github.com>
2017-07-25 09:40:17 +0900

    RowBounds/ResultHandlerのサンプルを追加

--- src/main/java/com/hainet/mybatis/spring/boot/sample/mapper/PersonMapper.java
+++ src/main/java/com/hainet/mybatis/spring/boot/sample/mapper/PersonMapper.java
@@ -3,6 +3,8 @@ package com.hainet.mybatis.spring.boot.sample.mapper;
 import com.hainet.mybatis.spring.boot.sample.domain.Person;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
 
 import java.util.List;
 
@@ -12,6 +14,10 @@ public interface PersonMapper {
     List<Person> findAllResultType();
 
     List<Person> findAll();
+
+    List<Person> findAllRowBounds(RowBounds rowBounds);
+
+    void findAllResultHandler(ResultHandler<Person> resultHandler);
 
     Person findById(int id);
 
