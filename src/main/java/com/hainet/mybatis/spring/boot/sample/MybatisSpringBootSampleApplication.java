56be0288360e08d7b0ae972155a9c3fb38c3f39e
hainet <20400616+hainet@users.noreply.github.com>
2017-10-15 22:51:22 +0900

    不要なimportを除去

--- src/main/java/com/hainet/mybatis/spring/boot/sample/MybatisSpringBootSampleApplication.java
+++ src/main/java/com/hainet/mybatis/spring/boot/sample/MybatisSpringBootSampleApplication.java
@@ -1,17 +1,7 @@
 package com.hainet.mybatis.spring.boot.sample;
 
-import com.hainet.mybatis.spring.boot.sample.domain.Person;
-import com.hainet.mybatis.spring.boot.sample.mapper.PersonMapper;
-import com.hainet.mybatis.spring.boot.sample.mapper.ConstructorPersonMapper;
-import org.apache.ibatis.cursor.Cursor;
-import org.apache.ibatis.session.RowBounds;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.io.IOException;
 
 @SpringBootApplication
 public class MybatisSpringBootSampleApplication  {
