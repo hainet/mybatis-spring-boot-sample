568178ef7d13cd0434c388e1a9e68e8c056b6d45
hainet <20400616+hainet@users.noreply.github.com>
2017-10-30 16:18:45 +0900

    typoの修正

--- src/main/java/com/hainet/mybatis/spring/boot/sample/MybatisSpringBootSampleApplication.java
+++ src/main/java/com/hainet/mybatis/spring/boot/sample/MybatisSpringBootSampleApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MybatisSpringBootSampleApplication  {
+public class MybatisSpringBootSampleApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MybatisSpringBootSampleApplication.class, args);
